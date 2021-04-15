8ecbb67171773aafa71f33ac2d6475287f95ea04
Xzzz918 <884146241@qq.com>
2021-04-15 21:56:06 +0800

    更新静态分派

---
+++ src/polymorphic/OverLoad.java
@@ -0,0 +1,42 @@
+package polymorphic;
+
+
+import java.io.Serializable;
+
+/**
+ * @author gemini
+ * Created in  2021/4/15 21:26
+ */
+public class OverLoad {
+    public static void sayHello(Object arg) {
+        System.out.println("hello object");
+    }
+
+    public static void sayHello(int arg) {
+        System.out.println("hello int");
+    }
+
+    public static void sayHello(long arg) {
+        System.out.println("hello long");
+    }
+
+    public static void sayHello(Character arg) {
+        System.out.println("hello character");
+    }
+
+    public static void sayHello(char arg) {
+        System.out.println("hello char");
+    }
+
+    public static void sayHello(char... arg) {
+        System.out.println("hello char...");
+    }
+
+    public static void sayHello(Serializable arg) {
+        System.out.println("hello Serializable");
+    }
+
+    public static void main(String[] args) {
+        sayHello('a');
+    }
+}
